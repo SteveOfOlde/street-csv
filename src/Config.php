2d099fbf6ce66fc164124d4050d07ca20ed1a752
samarqhand <samarqhand@users.noreply.github.com>
2025-05-12 19:52:55 +0100

    make config readonly

--- src/Config.php
+++ src/Config.php
@@ -4,5 +4,16 @@ namespace StreetCsv;
 
 class Config
 {
+    /** @var string[] */
+    public readonly array $titles;
+
+    /** @var string[] */
+    public readonly array $conjunctions;
+
+    public function __construct()
+    {
+        $this->titles = ['Mr', 'Mrs', 'Mister', 'Ms', 'Mx', 'Dr', 'Prof'];
+        $this->conjunctions = ['and', '&'];
+    }
 
 }
